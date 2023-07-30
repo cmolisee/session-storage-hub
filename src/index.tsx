77b1c89f839bf6dbc35cbd7e7e26a9c5e2209113
Cody Molisee <molisee.cody@gmail.com>
2023-07-30 14:30:29 -0700

    finished refactoring to mvp with some minor style differences and probably better performance and functionality.

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Popup from './views/popup';
+import { ToastContainer, Flip } from 'react-toastify';
+import 'react-toastify/scss/main.scss';
+
+const rootEle = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootEle);
+root.render(
+  <React.StrictMode>
+    <Popup />
+    <ToastContainer
+      position="bottom-center"
+      transition={Flip}
+      autoClose={500}
+      hideProgressBar
+      limit={1}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover
+      theme="colored" />
+  </React.StrictMode>
+);
