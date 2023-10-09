ce180dbdfb7a6ecf1249d70531cfe88d08901956
Cody Molisee <molisee.cody@gmail.com>
2023-10-08 21:19:10 -0700

    major: trial for major update

--- src/chromeServices/background.ts
+++ src/chromeServices/background.ts
@@ -2,18 +2,18 @@ export {};
 
 // install, extension update, or chrome update
 chrome.runtime.onInstalled.addListener((details) => {
-	console.log('[background] onInstalled', details);
+	console.debug('[background] onInstalled', details);
 });
 
 chrome.runtime.onConnect.addListener((port) => {
-	console.log('[background] onConnect', port);
+	console.debug('[background] onConnect', port);
 });
 
 chrome.runtime.onStartup.addListener(() => {
-	console.log('[background] onStartup');
+	console.debug('[background] onStartup');
 });
 
 // before unload
 chrome.runtime.onSuspend.addListener(() => {
-	console.log('[background] onSuspend');
+	console.debug('[background] onSuspend');
 });
