a5495e4f10f4849fc887ad9ff4908c5f5339b65e
codymolisee <cody.molisee@creditone.com>
2023-12-18 15:33:21 -0800

    update: fixed vite config to properly host at 8000

--- x/session-storage-hub/vite.config.ts
+++ x/session-storage-hub/vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  port: 8000,
-  watch: {
-    usePolling: true
+  server: {
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true
+    }
   }
 })
