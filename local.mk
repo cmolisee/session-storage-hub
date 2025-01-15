f64f288f58840a80c16db9486a759527e55dbb2a
Cody.Molisee <cody.molisee@creditone.com>
2025-01-14 16:32:32 -0800

    updated version.

--- local.mk
+++ local.mk
@@ -1,1 +1,13 @@
+TIGERS = Cody-Molisee,Dalen-Noice,Erick-Wakayu,Kaitlyn-Prerost
+BARDS = Eric-Feuerstein,Juan-Echevarria,Trixi-Jansuy,Kamila-Kinel
+SLINGERS = Harrison-Su,Kyle-Magdales,Arron-Abay
 
+labels = $(TIGERS),$(BARDS),$(SLINGERS)
+path = ~/Desktop/output.xlsx
+
+# jql based on this filter https://creditonebank.atlassian.net/issues/?filter=18890
+# make byMonth month=8 
+# 	optional: path=~/Desktop/output.xlsx
+#  	optional: labels=<all dev labels>
+byMonth: 
+	$(VENV)/bin/jkpy -s -j "\"Team Name[Dropdown]\" in (DPM-Web-Engineering, DPM-WE-Business-Bards, DPM-Code-Freeze, DPM-WE-Moose-Tigers, DPM-WE-Spell-Slingers) and status CHANGED TO (Closed, Deployed, \"Ready for Deployment\", Done, Completed) AFTER $(shell date -v$(month)m -v1d +%Y-%m-%d) BEFORE $(shell date -v$(month)m -v+1m -v1d -v-1d +%Y-%m-%d) ORDER BY created DESC" -p $(path) -l $(labels)
--- src/jkpy/__init__.py
+++ src/jkpy/__init__.py
@@ -2,6 +2,6 @@
 #  jkpy/__init__.py
 
 __app_name__ = "Jira KP(y)"
-__version__ = "0.1.0"
+__version__ = "1.1.0"
 
 from .cli import *
